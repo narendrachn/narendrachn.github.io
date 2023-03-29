475fbd4c34cb61080471a4f92377370b527da372
narendrachn <109662555+narendrachn@users.noreply.github.com>
2023-03-28 23:08:45 -0700

    Updating script in js file

--- Design system elements/javascript.js
+++ Design system elements/javascript.js
@@ -6,3 +6,18 @@
 //     // Display the results on the page
 //     document.getElementById("search-results").innerHTML = "<p>Search results for '" + searchQuery + "'</p>";
 //   }
+
+var acc = document.getElementsByClassName("comp-accordion");
+        var i;
+
+        for (i = 0; i < acc.length; i++) {
+          acc[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+              panel.style.display = "none";
+            } else {
+              panel.style.display = "block";
+            }
+          });
+        }
